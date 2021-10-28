64e3dae25c3f2216bbabfd880acc29c462288f01
Ambaza-Marcelo <84043779+Ambaza-Marcelo@users.noreply.github.com>
2021-10-29 01:47:01 +0200

    store function

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\DB;
 class ExpenseController extends Controller
 {
 
-    public function index()
+    //public function index()
     {
         //
        $expenseParMois = Expense::select(
